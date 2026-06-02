85008fa5d4a1f2539da3a50fd5b2ca7253f5e563
blujay <finkegabriel@gmail.com>
2026-06-02 12:55:02 -0700

    was not starting the libp2p node

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ async function run(conf = {}) {
   if (config.enableP2P) {
     console.log('Libp2p started with addresses:');
     node.start();
+    console.log(node);
     config.p2pAddress = node;
     debug('libp2p started with id', node.peerId.toString());
   }
